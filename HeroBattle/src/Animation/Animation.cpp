45e48453952b8cb25eb0a813ee825b36262d9ce1
thnh23 <lekimthanh27062k5@gmail.com>
2024-03-29 17:43:45 +0700

    update animation

---
+++ HeroBattle/src/Animation/Animation.cpp
@@ -0,0 +1,26 @@
+#include"Animation.h"
+#include"TextureManager.h"
+#include<iostream>
+void Animation::Update()
+{
+    m_SpriteFrame= (SDL_GetTicks()/m_AnimSpeed) % m_FrameCount;
+}
+
+void Animation::Draw(float x, float y, int spriteWidth, int spriteHeight)
+{
+TextureManager::GetInstance()->DrawFrame(m_TextureID,x,y,spriteWidth,spriteHeight,m_SpriteRow,m_SpriteFrame,m_Flip);
+}
+
+void Animation::setProps(std::string textureID, int spriteRow, int frame, int animSpeed, SDL_RendererFlip flip  )
+{
+    m_TextureID=textureID;
+    m_SpriteRow=spriteRow;
+    m_FrameCount=frame;
+    m_AnimSpeed=animSpeed;
+    m_Flip=flip;
+}
+
+// void Animation::check()
+// {
+//     std::cout<<m_TextureID<<" "<<m_SpriteRow<<" "<<m_FrameCount<<" "<<m_SpriteFrame<<" "<<m_AnimSpeed<<std::endl;
+// }
