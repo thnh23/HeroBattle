1c21e7e7b1dea1a99ed1a95296a5ccda346ebd89
thnh23 <lekimthanh27062k5@gmail.com>
2024-04-11 16:47:30 +0700

    add mouse input

--- HeroBattle/src/Inputs/Input.cpp
+++ HeroBattle/src/Inputs/Input.cpp
@@ -5,7 +5,14 @@ Input* Input::m_Instance = nullptr;
 Input::Input(){
     m_KeyStates =SDL_GetKeyboardState(nullptr);
 }
-
+void Input::init()
+{
+     for (int i = 0; i < 3; i++)
+     {
+          mouseButtonStates.push_back(false);
+     }
+     mPos = new Vector2D(0, 0);
+}
 void Input::Listen()
 {
     SDL_Event event;
@@ -18,19 +25,84 @@ void Input::Listen()
                 Engine::GetInstance()->Quit();
                 break;
             case SDL_KEYDOWN:
-                m_KeyStates =SDL_GetKeyboardState(nullptr);
+                m_KeyStates = SDL_GetKeyboardState(nullptr);
                 break;
             case SDL_KEYUP:
-               m_KeyStates =SDL_GetKeyboardState(nullptr);
+               m_KeyStates = SDL_GetKeyboardState(nullptr);
                 break;
+            case SDL_MOUSEMOTION:
+              onMouseMove(event);
+                break;
+           case SDL_MOUSEBUTTONDOWN:
+            onMouseButtonDown(event);
+            break;
+
+         case SDL_MOUSEBUTTONUP:
+            onMouseButtonUp(event);
+            break;
+
         }
     }
 }
 
 bool Input::GetKeyDown(SDL_Scancode key)
 {
-    return m_KeyStates[key] == 1;
+    return (m_KeyStates[key] == 1);
 }
+
+void Input::onMouseButtonDown(SDL_Event& e)
+{
+	if (e.button.button == SDL_BUTTON_LEFT)
+	{
+		mouseButtonStates[LEFT] = true;
+	}
+        else if (e.button.button = SDL_BUTTON_MIDDLE)
+	{
+		mouseButtonStates[MIDDLE] = true;
+	}
+        else if (e.button.button = SDL_BUTTON_RIGHT)
+	{
+		mouseButtonStates[RIGHT] = true;
+	}
+	   Engine::GetInstance()->getStateMachine()->onMouseButtonDown(e);
+}
+
+void Input::onMouseButtonUp(SDL_Event& e)
+{
+	if (e.button.button == SDL_BUTTON_LEFT)
+	{
+		mouseButtonStates[LEFT] = false;
+	}
+        else if (e.button.button == SDL_BUTTON_MIDDLE)
+	{
+		mouseButtonStates[MIDDLE] = false;
+	}
+        else if (e.button.button == SDL_BUTTON_RIGHT)
+	{
+		mouseButtonStates[RIGHT] = false;
+	}
+	Engine::GetInstance()->getStateMachine()->onMouseButtonUp(e);
+}
+
+void Input::onMouseMove(SDL_Event& e)
+{
+	mPos->setX(e.motion.x);
+	mPos->setY(e.motion.y);
+	Engine::GetInstance()->getStateMachine()->onMouseMove(e);
+}
+
+void Input::reset()
+{
+     for (int i = 0; i < mouseButtonStates.size(); i++)
+     {
+          mouseButtonStates[i] = false;
+     }
+}
+
+ bool Input::getMouseButtonState(int buttonNum)
+ {
+     return  mouseButtonStates[buttonNum];
+ }
 
 
 
--- HeroBattle/src/Inputs/Input.h
+++ HeroBattle/src/Inputs/Input.h
@@ -1,7 +1,16 @@
 #ifndef INPUT_H
 #define INPUT_H
 #include"SDL.h"
+#include "Vector2D.h"
+#include<vector>
+#include<iostream>
 
+enum mouse_buttons
+{
+     LEFT = 0,
+     MIDDLE = 1,
+     RIGHT = 2
+};
 class Input
 {
   public:
@@ -9,12 +18,33 @@ class Input
   {
     return m_Instance =(m_Instance !=nullptr)?m_Instance : new Input();
   }
+  void init();
   void Listen();
   bool GetKeyDown(SDL_Scancode key);
+  bool getMouseButtonState(int buttonNum);
+Vector2D* getMousePos()
+{
+     return mPos;
+}
+
+void reset();
+
+
   private:
      Input();
+      static Input* m_Instance;
+
+      //keyboard
      const Uint8* m_KeyStates;
-     static Input* m_Instance;
+    // mouse
+    std::vector<bool> mouseButtonStates;
+
+Vector2D* mPos;
+
+void onMouseMove(SDL_Event& e);
+void onMouseButtonDown(SDL_Event& e);
+void onMouseButtonUp(SDL_Event& e);
+
 };
 
 #endif
