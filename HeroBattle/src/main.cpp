0b72671fa84bd6e00190b35ce51148a96fe32d97
thnh23 <lekimthanh27062k5@gmail.com>
2024-04-11 16:46:31 +0700

    fix FPS

--- HeroBattle/src/main.cpp
+++ HeroBattle/src/main.cpp
@@ -1,19 +1,33 @@
 #include"Engine.h"
+#include <stdlib.h>
+#include<ctime>
+#include"Timer.h"
+#include<iostream>
 
+const float time_slice = 0.01f;
+float time_accumulator=0.0f;
+float time_last_update = Timer::GetInstance()->getTime();
 int main(int argc, char*argv[])
 {
+    srand(time(0));
     Engine::GetInstance()->Init();
 
     while(Engine::GetInstance()->IsRunning())
     {
-
-        Engine::GetInstance()->Events();
-        Engine::GetInstance()->Update();
+        float deltaTime = Timer::GetInstance()->getTime()- time_last_update;
+        //std::cout<<deltaTime<<std::endl;
+        time_last_update+=deltaTime;
+        time_accumulator+=deltaTime;
+        while(time_accumulator>=time_slice)
+        {
+            time_accumulator-=time_slice;
+            Engine::GetInstance()->Events();
+            Engine::GetInstance()->Update(deltaTime);
+        }
         Engine::GetInstance()->Render();
-
     }
-    Engine::GetInstance()->Clean();
 
+    Engine::GetInstance()->Clean();
     return 0;
 }
 
