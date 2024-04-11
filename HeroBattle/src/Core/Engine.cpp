3145fb368179635ae2939ccba4bbedcef4b5ff34
thnh23 <lekimthanh27062k5@gmail.com>
2024-04-11 16:50:33 +0700

    fix logic

--- HeroBattle/src/Core/Engine.cpp
+++ HeroBattle/src/Core/Engine.cpp
@@ -1,21 +1,30 @@
 #include"Engine.h"
 #include"TextureManager.h"
-#include"Knight.h"
 #include"Input.h"
-#include"SDL.h"
 #include<iostream>
 #include"MapParser.h"
 #include "Camera.h"
+#include"GamePlay.h"
+#include"HUD.h"
+#include"Button.h"
+#include"MenuState.h"
+
+
 Engine* Engine::m_Instance = nullptr;
-Knight* player = nullptr;
+
 
 bool Engine::Init()
 {
-  if(SDL_Init(SDL_INIT_VIDEO) !=0 && IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) !=0)
+  if(TTF_Init() !=0)
+  {
+      return false;
+  }
+  if(SDL_Init(SDL_INIT_EVERYTHING)<0)
   {
     SDL_Log("Failed to initialize SDL");
     return false;
   }
+
   m_Window = SDL_CreateWindow("Hero Batlle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT,0);
   if(m_Window== nullptr)
   {
@@ -28,39 +37,62 @@ bool Engine::Init()
     SDL_Log("Failed to creater renderer");
     return false;
   }
-  if(MapParser::GetInstance()->Load())
+  if(MapParser::GetInstance()->Load("Maps/Map.tmx","MAP"))
   {
       std::cout<<"Failed to load map" <<std::endl;
   }
   m_LevelMap = MapParser::GetInstance()->GetMap("MAP");
+  m_Font = TTF_OpenFont("Maps/MinimalPixelFont.ttf",20);
+  if(m_Font == nullptr)
+  {
+    SDL_Log("Failed to load font");
+    return false;
+  }
 
-   TextureManager::GetInstance()->Load("bg","Maps/country-platform-back.png");
+
+  TextureManager::GetInstance()->Load("bg","Maps/country-platform-back.png");
   TextureManager::GetInstance()->Load("knight","Image/Idle.png");
   TextureManager::GetInstance()->Load("knight_run","Image/Run.png");
   TextureManager::GetInstance()->Load("knight_jump","Image/Jump1.png");
   TextureManager::GetInstance()->Load("knight_fall","Image/Falling.png");
   TextureManager::GetInstance()->Load("knight_defend","Image/Defend.png");
   TextureManager::GetInstance()->Load("knight_attack1","Image/Attack 1.png");
+  TextureManager::GetInstance()->Load("knight_hurt","Image/Hurt.png");
+  TextureManager::GetInstance()->Load("enemy","Image/Skeleton_01_White_Idle.png");
+  TextureManager::GetInstance()->Load("enemy_run","Image/Skeleton_01_White_Walk.png");
+  TextureManager::GetInstance()->Load("enemy_attack","Image/Skeleton_01_White_Attack1.png");
+   TextureManager::GetInstance()->Load("enemy_hurt","Image/Skeleton_01_White_Hurt.png");
+   TextureManager::GetInstance()->Load("coin","Image/MonedaD.png");
+    TextureManager::GetInstance()->Load("playbutton","Image/start.png");
+    TextureManager::GetInstance()->Load("inforbutton","Image/information.png");
+    TextureManager::GetInstance()->Load("closebutton","Image/close.png");
+    TextureManager::GetInstance()->Load("shopbutton","Image/shop.png");
+    TextureManager::GetInstance()->Load("settingbutton","Image/setting.png");
+    TextureManager::GetInstance()->Load("menu_bg","Maps/Grassy_Mountains_preview_fullcolor.png");
+     TextureManager::GetInstance()->Load("resume_border","Image/border_02.png");
+     TextureManager::GetInstance()->Load("black_img","Maps/black_image.png");
+     TextureManager::GetInstance()->Load("continuebutton","Image/continue.png");
+     TextureManager::GetInstance()->Load("homebutton","Image/home.png");
+     TextureManager::GetInstance()->Load("restartbutton","Image/restart.png");
+  Input::GetInstance()->init();
+  HUD::GetInstance()->initHUD();
+GamePlay::GetInstance()->init();
+    Camera::GetInstance()->SetTarget(GamePlay::GetInstance()->getKnight()->GetOrigin());
+  gameStateMachine = new GameStateMachine();
+  gameStateMachine->pushState(new MenuState());
 
-
-  player = new Knight(new Properties("knight",600,424,72,86));
-
-  Camera::GetInstance()->SetTarget(player->GetOrigin());
  return m_IsRunning = true;
 }
 
-void Engine::Update()
+void Engine::Update(float dt)
 {
-  m_LevelMap->Update();
-  Camera::GetInstance()->Update(0.8);
-  player->Update(0.8);
+     gameStateMachine->update(dt);
 }
+
 void Engine::Render()
 {
-  SDL_RenderClear(m_Renderer);
-  TextureManager::GetInstance()->Draw("bg",0,0,1920,740);
-  m_LevelMap->Render();
-  player->Draw();
+    SDL_RenderClear(m_Renderer);
+     gameStateMachine->render();
   SDL_RenderPresent(m_Renderer);
 }
 
@@ -77,7 +109,9 @@ void Engine::Clean()
     SDL_DestroyWindow(m_Window);
     IMG_Quit();
     SDL_Quit();
+
 }
+
 
 void Engine::Quit()
 {
--- HeroBattle/src/Core/Engine.h
+++ HeroBattle/src/Core/Engine.h
@@ -1,12 +1,17 @@
+
 #ifndef ENGINE_H
 #define ENGINE_H
 
 #include"SDL.h"
 #include"SDL_image.h"
 #include"GameMap.h"
+#include"SDL_ttf.h"
+#include"GameStateMachine.h"
 
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 640;
+
+
 
 class Engine
 {
@@ -20,21 +25,25 @@ class Engine
    void Clean();
    void Quit();
 
-   void Update();
+   void Update(float dt);
    void Render();
    void Events();
    inline GameMap* GetMap(){return m_LevelMap;}
    inline bool IsRunning(){return m_IsRunning;}
    inline SDL_Renderer* getRenderer(){return m_Renderer;}
+   inline TTF_Font* getFont(){return m_Font;}
+   inline GameStateMachine* getStateMachine() { return gameStateMachine; }
 
   private:
     Engine(){};
-
     static Engine* m_Instance;
     bool m_IsRunning;
     GameMap* m_LevelMap;
     SDL_Window* m_Window;
     SDL_Renderer* m_Renderer;
+    TTF_Font* m_Font;
+    GameStateMachine* gameStateMachine;
+
 };
 
 #endif
